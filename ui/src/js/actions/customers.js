611ded3f993ecea4c393f3419c6378168cc67159
Girish <girishfriends.2012@gmail.com>
2018-02-26 09:58:27 +0530

    Revert "View Add Edit customers"
    This reverts commit 6293f32be77df83fbc7838330cdb6aad82094e37.

--- ui/src/js/actions/customers.js
+++ ui/src/js/actions/customers.js
@@ -1,29 +1,28 @@
-
 import axios from 'axios';
 
 
 export function createCustomer(customer) {
-  return axios.post('http://192.168.31.196:8080/v1/customers',  {...customer});
+  return axios.post('http://localhost:8080/v1/customers',  {...customer});
 }
 
 export function editCustomer(customer) {
-  return axios.put('http://192.168.31.196:8080/v1/customers',  {...customer});
+  return axios.put('http://localhost:8080/v1/customers',  {...customer});
 }
 
-export function deleteCustomer(id) {
-  return axios.delete('http://192.168.31.196:8080/v1/customers'+id);
+export function deleteCustomer() {
+  return axios.delete('http://localhost:8080/v1/customers'+ id);
 }
 
 
 export function getCustomer(id) {
-  return axios.get('http://192.168.31.196:8080/v1/customers/'+id);
+  return axios.get('http://localhost:8080/v1/customers/' + id);
 }
 
 
 export function allCustomers() {
-  return axios.get('http://192.168.31.196:8080/v1/customers');
+  return axios.get('http://localhost:8080/v1/customers/');
 }
 
 export function getCustomersWithPattern(pattern) {
-  return axios.get('http://192.168.31.196:8080/v1/filteredCustomers?searchPattern=' + pattern);
+  return axios.get('http://localhost:8080/v1/filteredCustomers?searchPattern=' + pattern);
 }
